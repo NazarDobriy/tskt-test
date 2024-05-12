49b0d1f218022536f99b2bd58cf9329a2741c901
Nazar <nazaranddobriy@gmail.com>
2024-05-12 23:24:42 +0300

    add static serve for fastapi

--- server/app/main.py
+++ server/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app import posts_router
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
